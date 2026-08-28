8a001e6aaced0a7a8423364b61beafccaaf70a68
aroslavpetrov <ya.a.petrov@gmail.com>
2026-08-28 10:48:30 +0300

    feat: update frontend to use Render backend URL

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     setMessage('Отправка данных на сервер...')
     
     try {
-      const res = await fetch('http://127.0.0.1:8000/api/v1/tenants/', {
+         const res = await fetch('https://compliance-box-backend.onrender.com/api/v1/tenants/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
